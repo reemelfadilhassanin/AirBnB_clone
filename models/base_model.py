46d52fa47f3e9347b0ddace05d276a8bdfd6d5ba
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-14 06:43:43 +0400

    init_storage

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
         else:
             for key, value in kwargs.items():
@@ -37,7 +38,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
-        models.storage.new(self)
+
         models.storage.save()
 
     def to_dict(self):
