ba1107bb42e216b325ab5d33e1c470431657cdde
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-14 07:07:40 +0400

    trail

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"""this module for auto initialize file storage
+"""
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+import os
 
-"""
-Initializes  the module global (singleton) variables
-"""
-
-from .engine.file_storage import FileStorage
-"""
-Retrieves the storage instance
-"""
 storage = FileStorage()
 storage.reload()
