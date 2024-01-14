4632c1e4c8d60ccf159e27fba28ce50d0910b706
hibakiz <hbosalah98sd@gmail.com>
2024-01-14 06:52:20 +0300

    classes __init

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """this module for auto initialize file storage
 """
+
+
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-import os
+
 
 storage = FileStorage()
 storage.reload()
