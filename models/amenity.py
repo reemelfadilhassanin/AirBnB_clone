c51a90220aebb825469af6ed0039e8a913ace3e6
hibakiz <hbosalah98sd@gmail.com>
2024-01-14 07:04:03 +0300

    classes doc

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-""" Amenity class module"""
+""" Amenity class module
+this class inhirted from BaseModel module
+to create a new new amenity objects
+while inheriting all properties of baseModel
+"""
 
-import uuid
-from datetime import datetime
-from models import storage
+
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-	"""Amenity class inhirted from BaseModel class"""
+	"""Amenity class inhirted from BaseModel class
+	with one public attribute name as an empty str
+	"""
 	name = ""
