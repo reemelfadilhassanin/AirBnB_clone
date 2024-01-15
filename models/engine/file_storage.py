e7c448d928aa03d3f52433bc175e40416d323831
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-15 05:40:10 +0400

    reload file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,14 +69,13 @@ Attributes:
         return classes
 
     def reload(self):
-        """Deserializes JSON file into __objects."""
+        """this to generate objects using deserlize"""
         if not os.path.isfile(FileStorage.__file_path):
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
